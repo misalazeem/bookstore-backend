bf8e473079c90c2c37c836186fc0aa05693b0295
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-10-26 19:36:51 +0500

    adding example env file for reference

--- src/auth/authGuard.ts
+++ src/auth/authGuard.ts
@@ -8,9 +8,7 @@ export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const AUTH_KEY = this.configService.get<string>('AUTH_KEY');
-    console.log(AUTH_KEY);
     const authHeader = request.headers['authorization'];
-    console.log(authHeader);
 
     if (!authHeader || !authHeader.startsWith('Bearer ') || authHeader.split(' ')[1] !== AUTH_KEY) {
       return false;
